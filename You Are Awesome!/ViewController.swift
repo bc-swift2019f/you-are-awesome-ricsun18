d44dce99dfc8dad6d0f8e94bc49ae289bb92fc47
Richard Sunden <djrap1dtecno@gmail.com>
2019-09-09 16:03:08 -0400

    Init

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Richard Sunden on 8/28/19.
+//  Copyright © 2019 Richard Sunden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
