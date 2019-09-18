5f60d831f801e29e79e4cbbd2dad6be53887d77f
Richard Sunden <djrap1dtecno@gmail.com>
2019-09-18 00:03:56 -0400

    2.17 Complete!

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var awesomeImageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
+    var awesomePlayer = AVAudioPlayer()
     var index = -1
     var imageIndex = -1
+    var soundIndex = -1
     let numberOfImages = 10
+    let numberOfSounds = 6
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,19 +39,42 @@ class ViewController: UIViewController {
  
         var newIndex: Int
         
+        //shows a message
         repeat {
             newIndex = Int.random(in: 0..<messages.count)
         } while index == newIndex
         
         index = newIndex
         messageLabel.text = messages[index]
-
+        
+        //shows an image
         repeat {
             newIndex = Int.random(in: 0..<numberOfImages)
         } while imageIndex == newIndex
         
         imageIndex = newIndex
         awesomeImageView.image = UIImage(named: "image\(newIndex)")
+        
+        //gets a random number to play in our soundName file
+        repeat {
+            newIndex = Int.random(in: 0..<numberOfSounds)
+        } while soundIndex == newIndex
+        
+        soundIndex = newIndex
+        
+        //plays a sound
+        var soundName = "sound\(soundIndex)"
+        
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try awesomePlayer = AVAudioPlayer(data: sound.data)
+                awesomePlayer.play()
+            } catch {
+                print("ERROR: data in \(soundName) couldn't be played as a sound")
+            }
+        } else {
+            print("Error: file \(soundName) didn't load")
+        }
         
     }
 }
